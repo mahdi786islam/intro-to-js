89d8b27745fc4fd16e01d052c012f97e1dd9206d
mahdi786islam <mahdiislam786@gmail.com>
2024-09-01 23:14:21 +0600

    got the flow back

--- Task1.js
+++ Task1.js
@@ -1,14 +1,29 @@
-var orange = 150;
-var apple = 250;
-var total = orange + apple;
+// var orange = 150;
+// var apple = 250;
+// var total = orange + apple;
 
+// console.log(total);
+
+// // var total = 500;
+
+// // var return = total - orange + apple;
+
+// // console.log(return)
+// var pocket = 500;
+// var back = pocket - total;
+// console.log(back);
+
+var math = 54.56;
+var physics = 45.44;
+var chemistry = 56.54;
+var biology = 54.67;
+var bangla = 45;
+
+var total = math + physics + chemistry + biology + bangla;
 console.log(total);
 
-// var total = 500;
+var avg = total % 5;
+console.log(avg.toFixed(3));
 
-// var return = total - orange + apple;
-
-// console.log(return)
-var pocket = 500;
-var back = pocket - total;
-console.log(back);
+var a = isNaN(2-10);
+console.log(a);
