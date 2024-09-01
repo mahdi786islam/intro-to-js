d03c8f02ac1fe83d8af2b72d76dbb3f577aff4bf
mahdi786islam <mahdiislam786@gmail.com>
2024-09-01 15:27:53 +0600

    first commit

---
+++ Task1.js
@@ -0,0 +1,14 @@
+var orange = 150;
+var apple = 250;
+var total = orange + apple;
+
+console.log(total);
+
+// var total = 500;
+
+// var return = total - orange + apple;
+
+// console.log(return)
+var pocket = 500;
+var back = pocket - total;
+console.log(back);
---
+++ array.js
@@ -0,0 +1,17 @@
+const number = [33, 44, 45, 56, 34, 45 ];
+
+console.log(number);
+console.log(number[2]);
+
+const second = number[1];
+console.log(second);
+
+const good = ['azad', 'rashed', 'azim'];
+good.push('latif');
+console.log(good);
+
+const lads = ['gufran', 'dalim', 'saif', 'sobhan'];
+lads.push('halim');
+console.log(lads);
+
+
